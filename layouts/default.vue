058e57bd9b2f52c0453406d0fcd2bb003febbf2b
orkhan.najafli <orkhan.najafli@digirella.az>
2024-10-09 09:41:16 +0400

    pm2

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,4 +5,10 @@
     <AppFooter />
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineComponent({
+  errorCaptured(err) {
+    return false; // Xətanı gizlətmək üçün false qaytarın
+  },
+});
+</script>
